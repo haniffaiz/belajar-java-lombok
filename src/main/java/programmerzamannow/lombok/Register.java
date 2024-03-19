26456edd305537cf319c030d226b48a1c15e78f6
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-20 01:38:28 +0700

    value

---
+++ src/main/java/programmerzamannow/lombok/Register.java
@@ -0,0 +1,14 @@
+package programmerzamannow.lombok;
+
+import lombok.Value;
+
+@Value
+//hasil akhir class final dan field final
+//immutable class
+public class Register {
+
+    String username;
+
+    String password;
+
+}
